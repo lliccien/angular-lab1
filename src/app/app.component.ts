bca9b189413109273bd2437b7dacc51ffb4b24a9
Ludwring Liccien <ludwring.liccien@gmail.com>
2018-02-10 14:44:16 -0500

    get user by id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { DirectoryService } from './services/directory.service';
-import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -10,8 +9,16 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AppComponent {
   users: any;
+  user: any;
 
   constructor( private directory: DirectoryService) {
    this.users = this.directory.getUsers();
+  //  this.directory.getUsers().subscribe( response => console.log(response));
   }
+
+  getUser(id) {
+    // this.user = this.directory.getUser(id).subscribe( response => console.log(response));
+   this.directory.getUser(id).subscribe(response => this.user = response);
+   }
+
 }
--- src/app/services/directory.service.ts
+++ src/app/services/directory.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 
 
 @Injectable()
@@ -8,9 +8,36 @@ export class DirectoryService {
   constructor( private firestore: AngularFirestore) { }
 
   getUsers() {
-    return this.firestore.collection('users').valueChanges();
+    // return this.firestore.collection('users').valueChanges();
+    const usersCollection = this.firestore.collection('users');
+    return usersCollection.snapshotChanges().map( actions => {
+      return actions.map( this.parserUser);
+    });
     }
 
+  getUser(id) {
+   //  return // this.firestore.collection('users').snapshotChanges().;
+    //doc(`${id}`).snapshotChanges().map( a => {
+      // debugger;
+    //   const data = a.payload.data;
+    //    return { id, ...data };
+    // });
+    return this.firestore.doc(`users/${id}`).valueChanges().map( a => {
+      //  debugger;
+      // console.log(a);
+       const data = a;
+        return { id, ...data };
+    });
+
+  }
+
+  parserUser(a)  {
+    const data = a.payload.doc.data();
+    const id = a.payload.doc.id;
+// console.log(data);
+// console.log(id);
+    return { id, ...data };
+  }
 }
 
 
